dea48a1e970df7b1511f7cfda94948c828265ad2
Anika-Fariha <anikafariha025@gmail.com>
2023-08-24 04:23:27 +0600

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/socials/tower.jpg";
+import image from "../images/tower.jpg";
 
-const imageAltText = "A staircase is the background image ";
+const imageAltText = "A staircase is the background image";
 
 const Home = ({ name, title }) => {
   return (
